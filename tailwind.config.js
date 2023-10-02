4f8ecca5eb2f62e0d5c80e61fce3fb652f46c139
rupom <mohrajulrupom@gmail.com>
2023-10-02 07:06:59 -0400

    add

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['popular-project.html','project-started.html','property-type.html',
+  'email-add.html','phone.html','name.html','constructors-bit.html','constructor-need.html',
+  'relationship-property.html','your-budget.html','description-project.html',
+  './src/**/*.{html,js}',],
+  theme: {
+    extend: {
+      
+
+    },
+  },
+
+  plugins: [],
+ 
+}
+
